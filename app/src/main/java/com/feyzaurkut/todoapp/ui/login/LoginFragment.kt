dd9113dfa43b0bd05170991e68d535003f96fe22
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-12 15:59:53 +0300

    Add login process

--- app/src/main/java/com/feyzaurkut/todoapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/feyzaurkut/todoapp/ui/login/LoginFragment.kt
@@ -1,21 +1,66 @@
 package com.feyzaurkut.todoapp.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.feyzaurkut.todoapp.R
+import com.feyzaurkut.todoapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+
+        auth = Firebase.auth
+        initListeners()
+
+        return binding.root
+    }
+
+    private fun initListeners() {
+
+        with(binding) {
+            btnSignIn.setOnClickListener {
+                signIn()
+            }
+            tvSignUp.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+        }
+    }
+
+    private fun signIn() {
+
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Authentication success.", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    Log.e("SignInFailed", task.exception.toString())
+                    Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    binding.etEmail.setText("")
+                    binding.etPassword.setText("")
+                }
+            }
     }
 
 }
