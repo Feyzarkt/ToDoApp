c1f94158a7f2800a6426d1187157760935ffc8a0
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-12 16:00:46 +0300

    Add register process

--- app/src/main/java/com/feyzaurkut/todoapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/feyzaurkut/todoapp/ui/register/RegisterFragment.kt
@@ -1,20 +1,65 @@
 package com.feyzaurkut.todoapp.ui.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.feyzaurkut.todoapp.R
+import com.feyzaurkut.todoapp.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater)
+
+        auth = Firebase.auth
+        initListeners()
+
+        return binding.root
     }
 
+    private fun initListeners() {
+
+        with(binding) {
+            btnSignUp.setOnClickListener {
+                createNewAccount()
+            }
+            tvSignIn.setOnClickListener {
+                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+        }
+    }
+
+    private fun createNewAccount() {
+
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Authentication success.", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+                } else {
+                    Log.e("RegisterFailed", task.exception.toString())
+                    Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    binding.etUsername.setText("")
+                    binding.etEmail.setText("")
+                    binding.etPassword.setText("")
+                }
+            }
+    }
 }
