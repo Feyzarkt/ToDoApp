413c9c58adce31f698a57524c3c1b95c18a2ab77
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-12 15:58:50 +0300

    Check if user login

--- app/src/main/java/com/feyzaurkut/todoapp/MainActivity.kt
+++ app/src/main/java/com/feyzaurkut/todoapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.feyzaurkut.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            navController.navigate(R.id.homeFragment)
+        }
+        else navController.navigate(R.id.loginFragment)
     }
 }
