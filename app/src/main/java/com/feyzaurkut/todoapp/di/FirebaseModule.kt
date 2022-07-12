81213ff3fea67697188bff3ca08b949f865e415c
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-12 17:30:16 +0300

    Create firebase module

---
+++ app/src/main/java/com/feyzaurkut/todoapp/di/FirebaseModule.kt
@@ -0,0 +1,30 @@
+package com.feyzaurkut.todoapp.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestoreInstance(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuthInstance(): FirebaseAuth {
+        return Firebase.auth
+    }
+
+}
