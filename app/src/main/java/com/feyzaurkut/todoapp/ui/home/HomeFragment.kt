4e8c8e7bdd0162786791d4172872613c7bf67d73
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-12 15:59:14 +0300

    Add logout button

--- app/src/main/java/com/feyzaurkut/todoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/feyzaurkut/todoapp/ui/home/HomeFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.feyzaurkut.todoapp.R
+import com.feyzaurkut.todoapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+
+        auth = Firebase.auth
+        initListeners()
+
+        return binding.root
+    }
+
+    private fun initListeners() {
+        with(binding) {
+            ivLogout.setOnClickListener {
+                auth.signOut()
+                findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+            }
+        }
     }
 
 }
