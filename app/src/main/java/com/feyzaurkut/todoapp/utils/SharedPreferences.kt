f5dd39d1e81d8e12732d37c3bd0c915975cda6ee
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-14 13:09:40 +0300

    Add shared preferences

---
+++ app/src/main/java/com/feyzaurkut/todoapp/utils/SharedPreferences.kt
@@ -0,0 +1,32 @@
+package com.feyzaurkut.todoapp.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferences(private val context: Context) {
+
+    private fun createPreferences(): SharedPreferences? {
+        return context.getSharedPreferences(Constants.FILE_NAME, Context.MODE_PRIVATE)
+    }
+
+    private fun createEditor(): SharedPreferences.Editor? {
+        val preferences = createPreferences()
+        return preferences?.edit()
+    }
+
+    fun putColorInt(key: String, value: Int){
+        createEditor()?.putInt(key, value)?.apply()
+    }
+
+    fun getColorInt(key: String): Int{
+        return createPreferences()?.getInt(key, -16777216) ?: -16777216
+    }
+
+    fun putUsernameString(value: String){
+        createEditor()?.putString(Constants.USERNAME, value)?.apply()
+    }
+
+    fun getUsernameString(): String{
+        return createPreferences()?.getString(Constants.USERNAME, "") ?: ""
+    }
+}
